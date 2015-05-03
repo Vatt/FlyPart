29a8ebb2614a530af9651e9e72189dc7b0dfaf15
Vatt <protossgamover@gmail.com>
2015-05-04 00:08:36 +0500

    .

--- FlyPartEngine/fpSystem/Memory/fpStoringAllocator.h
+++ FlyPartEngine/fpSystem/Memory/fpStoringAllocator.h
@@ -2,39 +2,63 @@
 #define _FP_STORING_ALLOCATOR_
 #include <stddef.h>
 #include "fpAllocator.h"
-#include "../fpSystemInterface.h"
 class fpStoringAllocator: public fpAllocator
 {
-    enum {PAGES_IN_POOL = 8};
+    enum {PAGES_IN_POOL = 4};
 	
 	struct MemPage{
-		void* firstFreePtr;
+		char* firstFreePtr;
         void* firstPtr;
-		uint32 used;
+		uint32 used; 
 		uint32 avail;
     };
+	struct FreeMem
+	{
 
+	};
     struct MemPool
     {
 		uint32 pageSize;
-		uint32 granularity;
         uint32 total;
         uint32 used;
         uint32 avail;
+		uint16 pageIndex;
+		
 		MemPage* pages[];
 		MemPool()
 		{
 			 pageSize = fpMemoryStats::PageSize();
-			 granularity = fpMemoryStats::Granularity();
-			// pages[] = (MemPage*)platform->SystemAlloc(sizeof(MemPage)*PAGES_IN_POOL);
 			 for (int16 index = 0;index < PAGES_IN_POOL;index++)
 			 {
 				 pages[index] = (MemPage*)platform->SystemAlloc(sizeof(MemPage));
 				 pages[index]->avail = pageSize;
 				 pages[index]->firstPtr = platform->SystemAlloc(pageSize);
-				 pages[index]->firstFreePtr = pages[index]->firstPtr;
+				 pages[index]->firstFreePtr = (char*)pages[index]->firstPtr;
 				 pages[index]->used = 0;
 			 }
+			 pageIndex = 0;
+			 total = pageSize * PAGES_IN_POOL;
+			 used = 0;
+			 avail = total;
+		}
+		void* getMem(uint32  size)
+		{
+			void* retPtr = nullptr;
+			if (pages[pageIndex]->avail >= size)
+			{
+				retPtr =  pages[pageIndex]->firstFreePtr;
+				pages[pageIndex]->used += size;
+				pages[pageIndex]->avail -= size;
+				pages[pageIndex]->firstFreePtr += size;
+			}
+			if (++pageIndex < PAGES_IN_POOL)
+			{
+				retPtr = pages[pageIndex]->firstFreePtr;
+				pages[pageIndex]->used += size;
+				pages[pageIndex]->avail -= size;
+				pages[pageIndex]->firstFreePtr += size;
+			}
+			return retPtr;
 		}
 
     };
